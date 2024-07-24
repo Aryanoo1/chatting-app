932b572be00135d89617b9a1f089d6aa77198c6e
Aryanoo1 <96529828+Aryanoo1@users.noreply.github.com>
2024-07-25 04:09:08 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -10,7 +10,9 @@ const { app, server } = require('./socket/index')
 console.log(process.env.FRONTEND_URL)
 app.use(cors({
     origin : process.env.FRONTEND_URL,
-    credentials : true
+    credentials : true,
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type']
 }))
 app.use(express.json())
 app.use(cookiesParser())
